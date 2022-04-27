879e31bcc5e60925417315bbe4db39da438288cd
ruhulaminbd93 <ruhulaminbd93@gmail.com>
2022-04-27 20:14:07 +0600

    location list

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import Footer from '../common/Footer';
-import Header from '../common/Header';
-import Category from './category/Category';
-import SearchBanner from './searchBanner/SearchBanner';
+import React from "react";
+import Location from "../../components/Location/Location";
+import Footer from "../common/Footer";
+import Header from "../common/Header";
+import Category from "./category/Category";
+import SearchBanner from "./searchBanner/SearchBanner";
 
 const Home = () => {
   return (
     <div>
-      <div className='bg-success'>
+      <div className="bg-success">
         <Header></Header>
         <SearchBanner></SearchBanner>
       </div>
+      <Location />
       <Category></Category>
       <Footer></Footer>
     </div>
--- src/Pages/Home/category/Category.jsx
+++ src/Pages/Home/category/Category.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const Category = () => {
-     return (
-          <div>
-               <h1>this is gategory</h1>
-          </div>
-     );
+  return (
+    <div>
+      <h1>this is category</h1>
+    </div>
+  );
 };
 
 export default Category;
