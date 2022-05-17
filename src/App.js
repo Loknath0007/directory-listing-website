dd3c4f60f82c9e3e09eb042b3310934231c81b4e
unknown <mntanveer9196@gmail.com>
2022-05-17 12:26:09 +0600

    route added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import "./App.css";
-import { Route, Routes } from "react-router-dom";
-import Home from "./Pages/Home/Home";
-import Dashboard from "./Pages/Dashboard/Dashboard";
-import NotFound from "./Pages/NotFound/NotFound";
-import Headers from "./Pages/Dashboard/Headers/Headers";
-import PostAds from "./Pages/Dashboard/PostAds/PostAds";
-import SearchAds from "./Pages/Dashboard/SearchAds/SearchAds";
-import Categories from "./Pages/Dashboard/Categories/Categories";
-import ManageLocations from "./Pages/Dashboard/ManageLocations/ManageLocations";
-import Login from "./Pages/Login/Login";
-import Registration from "./Pages/Registration/Registration";
-import AboutUs from "./Pages/AboutUs/AboutUs";
-import ContactUs from "./Pages/ContactUs/ContactUs";
-import SingleAdDetail from "./Pages/SingleAdDetail/SingleAdDetail";
-import Ads from "./components/Ads/Ads";
-import Footers from "./Pages/Dashboard/Footers/Footers";
-import FooterDetails from "./Pages/Dashboard/Footers/FooterDetails";
-import Pages from "./Pages/Dashboard/DashboardPages/Pages";
-import AddNewPage from "./Pages/Dashboard/DashboardPages/AddNewPage";
-import SubFooter from "./Pages/Dashboard/Footers/SubFooter";
-import ColOne from "./Pages/Dashboard/Footers/Columns/ColOne";
-import UserProfile from "./Pages/Dashboard/UserProfile/UserProfile";
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import Dashboard from './Pages/Dashboard/Dashboard';
+import NotFound from './Pages/NotFound/NotFound';
+import Headers from './Pages/Dashboard/Headers/Headers';
+import PostAds from './Pages/Dashboard/PostAds/PostAds';
+import SearchAds from './Pages/Dashboard/SearchAds/SearchAds';
+import Categories from './Pages/Dashboard/Categories/Categories';
+import ManageLocations from './Pages/Dashboard/ManageLocations/ManageLocations';
+import Login from './Pages/Login/Login';
+import Registration from './Pages/Registration/Registration';
+import AboutUs from './Pages/AboutUs/AboutUs';
+import ContactUs from './Pages/ContactUs/ContactUs';
+import SingleAdDetail from './Pages/SingleAdDetail/SingleAdDetail';
+import Ads from './components/Ads/Ads';
+import Footers from './Pages/Dashboard/Footers/Footers';
+import FooterDetails from './Pages/Dashboard/Footers/FooterDetails';
+import Pages from './Pages/Dashboard/DashboardPages/Pages';
+import AddNewPage from './Pages/Dashboard/DashboardPages/AddNewPage';
+import SubFooter from './Pages/Dashboard/Footers/SubFooter';
+import ColOne from './Pages/Dashboard/Footers/Columns/ColOne';
+import UserProfile from './Pages/Dashboard/UserProfile/UserProfile';
+import Profile from './Pages/Profile';
 
 function App() {
   return (
@@ -34,9 +35,7 @@ function App() {
         <Route path="pages" element={<Pages />} />
         <Route path="categories" element={<Categories />} />
         <Route path="manage-locations" element={<ManageLocations />} />
-        <Route path="footers" element={<Footers />}>
-          
-        </Route>
+        <Route path="footers" element={<Footers />}></Route>
         <Route path="detail" element={<FooterDetails />}></Route>
         <Route path={`columns/:col`} element={<ColOne />} />
         <Route path="subFooter" element={<SubFooter />} />
@@ -50,6 +49,10 @@ function App() {
       <Route path="about" element={<AboutUs />} />
       <Route path="contact" element={<ContactUs />} />
       <Route path="*" element={<NotFound />} />
+
+      <Route path="profile" element={<Profile />}>
+        {/* <Route path="" element={<Login />} /> */}
+      </Route>
     </Routes>
   );
 }
