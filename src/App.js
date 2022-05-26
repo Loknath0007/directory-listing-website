663b95cea6e4c3941f59e91eb2056e94ca9e73cd
unknown <mntanveer9196@gmail.com>
2022-05-26 20:55:40 +0600

    location handled

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
-import "./App.css";
-import { Route, Routes } from "react-router-dom";
-import Home from "./Pages/Home/Home";
-import Dashboard from "./Pages/Dashboard/Dashboard";
-import NotFound from "./Pages/NotFound/NotFound";
-import Headers from "./Pages/Dashboard/Headers/Headers";
-import PostAds from "./Pages/Dashboard/PostAds/PostAds";
-import SearchAds from "./Pages/Dashboard/SearchAds/SearchAds";
-import Categories from "./Pages/Dashboard/Categories";
-import ManageLocations from "./Pages/Dashboard/ManageLocations/ManageLocations";
-import Registration from "./Pages/Registration/Registration";
-import AboutUs from "./Pages/AboutUs/AboutUs";
-import ContactUs from "./Pages/ContactUs/ContactUs";
-import SingleAdDetail from "./Pages/SingleAdDetail/SingleAdDetail";
-import Ads from "./components/Ads/Ads";
-import Footers from "./Pages/Dashboard/Footers/Footers";
-import FooterDetails from "./Pages/Dashboard/Footers/FooterDetails";
-import Pages from "./Pages/Dashboard/DashboardPages/Pages";
-import AddNewPage from "./Pages/Dashboard/DashboardPages/AddNewPage";
-import SubFooter from "./Pages/Dashboard/Footers/SubFooter";
-import ColOne from "./Pages/Dashboard/Footers/Columns/ColOne";
-import AdminProfile from "./Pages/Dashboard/Profile/AdminProfile";
-import DashboardHome from "./Pages/Dashboard/DashboardHome/DashboardHome";
-import Packages from "./Pages/Dashboard/Packages/Packages";
-import Payment from "./Pages/Dashboard/Payment/Payment";
-import Users from "./Pages/Dashboard/Users/Users";
-import Settings from "./Pages/Dashboard/Settings/Settings";
-import Location from "./Pages/Location/Location";
-import Profile from "./Pages/Profile";
-import ProfileHome from "./Pages/Profile/ProfileHome";
-import BuyCredits from "./Pages/Profile/BuyCredits";
-import VerifyBadge from "./Pages/Profile/VerifyBadge";
-import ManageAds from "./Pages/Profile/ManageAds";
-import Login from "./Pages/Login/Login";
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import Dashboard from './Pages/Dashboard/Dashboard';
+import NotFound from './Pages/NotFound/NotFound';
+import Headers from './Pages/Dashboard/Headers/Headers';
+import PostAds from './Pages/Dashboard/PostAds/PostAds';
+import SearchAds from './Pages/Dashboard/SearchAds/SearchAds';
+import Categories from './Pages/Dashboard/Categories';
+import Registration from './Pages/Registration/Registration';
+import AboutUs from './Pages/AboutUs/AboutUs';
+import ContactUs from './Pages/ContactUs/ContactUs';
+import SingleAdDetail from './Pages/SingleAdDetail/SingleAdDetail';
+import Ads from './components/Ads/Ads';
+import Footers from './Pages/Dashboard/Footers/Footers';
+import FooterDetails from './Pages/Dashboard/Footers/FooterDetails';
+import Pages from './Pages/Dashboard/DashboardPages/Pages';
+import AddNewPage from './Pages/Dashboard/DashboardPages/AddNewPage';
+import SubFooter from './Pages/Dashboard/Footers/SubFooter';
+import ColOne from './Pages/Dashboard/Footers/Columns/ColOne';
+import AdminProfile from './Pages/Dashboard/Profile/AdminProfile';
+import DashboardHome from './Pages/Dashboard/DashboardHome/DashboardHome';
+import Packages from './Pages/Dashboard/Packages/Packages';
+import Payment from './Pages/Dashboard/Payment/Payment';
+import Users from './Pages/Dashboard/Users/Users';
+import Settings from './Pages/Dashboard/Settings/Settings';
+import Location from './Pages/Location/Location';
+import Profile from './Pages/Profile';
+import ProfileHome from './Pages/Profile/ProfileHome';
+import BuyCredits from './Pages/Profile/BuyCredits';
+import VerifyBadge from './Pages/Profile/VerifyBadge';
+import ManageAds from './Pages/Profile/ManageAds';
+import Login from './Pages/Login/Login';
+import Locations from './Pages/Dashboard/Locations';
 
 function App() {
   return (
@@ -46,7 +46,7 @@ function App() {
         <Route path="users" element={<Users />} />
         <Route path="pages" element={<Pages />} />
         <Route path="categories" element={<Categories />} />
-        <Route path="manage-locations" element={<ManageLocations />} />
+        <Route path="manage-locations" element={<Locations />} />
         <Route path="payment" element={<Payment />} />
         <Route path="packages" element={<Packages />} />
         <Route path="footers" element={<Footers />}></Route>
